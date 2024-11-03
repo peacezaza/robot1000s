f15f442775ae0c8a6bbc3914b4ee9a90deb7172d
Kanisorn <32830583+peacezaza@users.noreply.github.com>
2024-11-03 13:36:13 +0700

    Update sketch_oct14a.ino

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -3,10 +3,13 @@
   Date:8/7/2024 -> Present
 */
 
+
+
 //import Library
 #include <Wire.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 //End import Library
 
@@ -26,24 +29,14 @@
 #define ENCODER_PIN 25
 #define ENCODER_PINBACK 4
 #define DISTANCE_THRESHOLD 30
-
-
-//End Define Vaiable SECTION
-
-//Define MQTT BROKER
+#define distanceCM_cal(x) ((x * SOUND_SPEED) / 2);
 WiFiClient client;
 PubSubClient mqtt(client);
 //End Define MQTT BROKER
 
 //global variable section
-hw_timer_t* timer = NULL;
 bool isToggle = true;
-volatile unsigned long pulseCount = 0;
-volatile unsigned long pulseCountBack = 0;
-unsigned long lastTime = 0;
-float rpm = 0;
-float rpmBack = 0;
-unsigned long timeInterval = 1000;
+bool messageSent = false;
 unsigned long currentTime;
 float distanceCm;
 float distanceCmRight;
@@ -54,17 +47,14 @@ int lastSaveTime2 = 0;
 int countRotate = 1;
 String direction;
 int distance;
+unsigned long timeUse = 0;
+bool toggle = false;
 
-String ValueTopic = "/ESP32/ultraSonicFront";
-String ValueTopic2 = "/ESP32/ultraSonicRight";
+
 String robotDirection = "/ESP32/direction";
+String toogleRobot = "/ESP32/toggle";
+String statusRobot ="/ESP32/status";
 
-
-
-//End global variable section
-void IRAM_ATTR onTimer();
-void IRAM_ATTR encoderISR();
-void IRAM_ATTR encoderISRBack();
 
 void setDirectionOutputPortA();
 void setDirectionOutputPortB();
@@ -79,11 +69,9 @@ void back_motorStop();
 void front_motorTurnleft();
 void back_motorTurnleft();
 void publishMessage(String Topic, String message);
-void callback(char* topic, byte* message, unsigned int length);
 void getMotorSpeedFrontLeft();
 void getMotorSpeedBackRight();
-void onTimer();
-void calculateDistanceBack();
+
 
 void setup() {
   Serial.begin(115200);
@@ -105,139 +93,95 @@ void setup() {
   // attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), encoderISR, RISING);
   // attachInterrupt(digitalPinToInterrupt(ENCODER_PINBACK), encoderISRBack, RISING);
 
-  // timer = timerBegin(10000);
-  // timerAttachInterrupt(timer, &onTimer);
-  // timerAlarm(timer,500, true, 0);
-
   // MQTT Setup Section
   connectWiFi();
+  isToggle = false;
 
-  //End MQTT Setup Section
-
-  // WiFi.mode(WIFI_STA);
-  // WiFi.disconnect();  // Disconnect from any existing WiFi network
-
-  // delay(100);
-  // Serial.println("Scanning for available Wi-Fi networks...");
-
-  // int numberOfNetworks = WiFi.scanNetworks();  // Perform the Wi-Fi scan
-
-  // Print results of the scan
-  // if (numberOfNetworks == 0) {
-  //   Serial.println("No networks found");
-  // } else {
-  //   Serial.print("Found ");
-  //   Serial.print(numberOfNetworks);
-  //   Serial.println(" networks:");
-
-  //   for (int i = 0; i < numberOfNetworks; i++) {
-  //     // Print details of each network found
-  //     Serial.print(i + 1);
-  //     Serial.print(": ");
-  //     Serial.print(WiFi.SSID(i));            // Print SSID (network name)
-  //     Serial.print(" (RSSI: ");
-  //     Serial.print(WiFi.RSSI(i));            // Print signal strength
-  //     Serial.print(" dBm) ");
-  //     Serial.print(" [Channel: ");
-  //     Serial.print(WiFi.channel(i));         // Print channel number
-  //     Serial.print("] ");
-
-  //     // Check if it's an open or secured network
-  //     Serial.print((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? "Open" : "Secured");
-  //     Serial.println();
-  //   }
-  // }
 }
-
 
 //
 void loop() {
   mqttConnect();
-  // currentTime = millis();
-  // if (currentTime - lastTime >= timeInterval) {
 
-  //   getMotorSpeedFrontLeft();
-  //   getMotorSpeedBackRight();
 
-  //   lastTime = currentTime;
-  // }
 
-  ultraRead_Front();
 
-  // currentTime2 = millis();
-  // if (currentTime2 - lastSaveTime2 >= 100) {
-  //   lastSaveTime2 = currentTime2;
-  //   // ultraRead_Front();
-  //   Serial.println("Ultra Read");
-  //   if (distanceCm > DISTANCE_THRESHOLD) {
-  //     front_motorForward();
-  //     back_motorForward();
-  //   } else {
-  //     front_motorStop();
-  //     back_motorStop();
-  //     delay(3000);
-  //     front_motorTurnleft();
-  //     back_motorTurnleft();
-  //     delay(2000);
-  //     countRotate++;
-  //     if (countRotate % 4 == 1) {
-  //       direction = "North";
-  //     } else if (countRotate % 4 == 2) {
-  //       direction = "West";
-  //     } else if (countRotate % 4 == 3) {
-  //       direction = "South";
-  //     } else if (countRotate % 4 == 0) {
-  //       direction = "East";
-  //     }
-  //     // publishMessage(robotDirection, direction);
-  //     // publishMessage(robotDirection, direction);
-  //   }
+  if(isToggle){
+    if(isToggle != toggle){
+      lastSaveTime2 = millis();
+      timeUse = millis();
+      toggle = true;
+    }
+    if (millis() - lastSaveTime2 >= 500) {
+      ultraRead_Front();
+      if (distanceCm > DISTANCE_THRESHOLD) {
+        front_motorForward();
+        back_motorForward();
+        messageSent = false;
+      } else {
+        Serial.println("STOP!");
+        countRotate++;
+        if (countRotate % 4 == 1) {
+          direction = "North";
+        } else if (countRotate % 4 == 2) {
+          direction = "West";
+        } else if (countRotate % 4 == 3) {
+          direction = "South";
+        } else if (countRotate % 4 == 0) {
+          direction = "East";
+        }
+        front_motorStop();
+        back_motorStop();
+        // mqttConnect();
 
-    if (distanceCm > DISTANCE_THRESHOLD) {
-      front_motorForward();
-      back_motorForward();
+        if(!messageSent && mqtt.connected()){
+          DynamicJsonDocument doc(1024);
+          doc["direction"] = direction;
+          doc["time"] = (millis() - timeUse)/1000;
+          // mqtt.publish(robotDirection.c_str(), direction.c_str());
+          // mqtt.publish("/ESP32/time", String(.c_str());
+          char buffer[256];
+          serializeJson(doc,buffer);
+          mqtt.publish(robotDirection.c_str(), buffer);
+          messageSent = true;
+          // publishMessage(robotDirection, direction);
+        }
+
+        front_motorTurnleft();
+        back_motorTurnleft();
+        delay(1700);
+        timeUse = millis();
+      }
+      lastSaveTime2 = millis();
+      Serial.print("Direction : ");
+      Serial.println(direction);
+
     }
 
-
-
-
-
-
-
-  // ultraRead_Front();
-  // publishMessage(ValueTopic, String(distanceCm));
-  // ultraRead_Right();
-  // ultraRead_Front();
-  // publishMessage(ValueTopic, String(distanceCm));
-  // front_motorForward();
-  // back_motorForward();
-
-
-  // if (distanceCm > DISTANCE_THRESHOLD) {
-  //   front_motorForward();
-  //   back_motorForward();
-  // } else {
-  //   front_motorStop();
-  //   back_motorStop();
-  //   delay(200);
-  //   front_motorTurnleft();
-  //   back_motorTurnleft();
-  //   delay(2000);
-  //   countRotate++;
-  //   if (countRotate % 4 == 1) {
-  //     direction = "North";
-  //   } else if (countRotate % 4 == 2) {
-  //     direction = "West";
-  //   } else if (countRotate % 4 == 3) {
-  //     direction = "South";
-  //   } else if (countRotate % 4 == 0) {
-  //     direction = "East";
-  //   }
-  //   publishMessage(robotDirection, direction);
-  //   publishMessage(robotDirection, direction);
-  // }
-
+  }else{
+    toggle = false;
   }
+
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  String topic_str = topic, payload_str = (char*)payload;
+  Serial.print("Payload is :");
+  Serial.println("[" + topic_str + "]: " + payload_str);
+  mqttConnect();
+  if(payload_str == "0"){
+    isToggle = false;
+    publishMessage(statusRobot, "Stopped");
+    front_motorStop();
+    back_motorStop();
+  }
+  else if(payload_str == "1"){
+    isToggle =true;
+      publishMessage(statusRobot, "Running");
+  }
+}
 
 
 
@@ -256,7 +200,7 @@ void mqttConnect() {
     if (mqtt.connect(MQTT_NAME, MQTT_USERNAME,
                      MQTT_PASSWORD)) {
       Serial.println("connected");
-      mqtt.subscribe("/ESP32_1/STATUS");
+      mqtt.subscribe("/ESP32/toggle");
     } else {
       Serial.println("failed");
       delay(5000);
@@ -266,29 +210,7 @@ void mqttConnect() {
   }
 }
 
-//callback function for mqttbroker
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
 
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-  if (String(topic) == "/ESP32_1/STATUS") {
-    Serial.print("Changing output to ");
-    if (messageTemp == "toggle") {
-      Serial.println("Motor is Toggle!");
-      isToggle = true;
-    } else {
-      Serial.println("Motor is Stop!");
-      isToggle = false;
-    }
-  }
-}
 
 void connectWiFi() {
   WiFi.mode(WIFI_STA);  //Optional
@@ -306,12 +228,6 @@ void connectWiFi() {
   mqtt.setCallback(callback);
 }
 
-void IRAM_ATTR encoderISR() {
-  pulseCount++;
-}
-void IRAM_ATTR encoderISRBack() {
-  pulseCountBack++;
-}
 
 void setDirectionOutputPortA() {
   Wire.beginTransmission(MCP23017);
@@ -394,9 +310,6 @@ void back_motorTurnRight() {
   Wire.endTransmission();
 }
 
-float distanceCM_cal(long duration) {
-  return duration * SOUND_SPEED / 2;
-}
 void ultraRead_Front() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -406,7 +319,7 @@ void ultraRead_Front() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+  
   long duration = pulseIn(echoPin, HIGH);
 
   // Calculate the distance
@@ -417,6 +330,7 @@ void ultraRead_Front() {
   Serial.print("Distance Front (cm): ");
   Serial.println(distanceCm);
   delay(100);
+
 }
 
 void ultraRead_Right() {
@@ -441,39 +355,10 @@ void ultraRead_Right() {
   delay(100);
 }
 
-void getMotorSpeedFrontLeft() {
-  float revolutions = pulseCount / 20.0;
-  rpm = (revolutions / (timeInterval / 1000.0)) * 60.0;  // Calculate RPM
-  pulseCount = 0;
-
-  Serial.print("Motor Speed: ");
-  Serial.print(rpm);
-  Serial.println(" RPM");
-}
-
-void getMotorSpeedBackRight() {
-  float revolutionsBack = pulseCountBack / 20.0;
-  rpmBack = (revolutionsBack / (timeInterval / 1000.0)) * 60.0;  // Calculate RPM
-
-  // Reset pulse count
-  pulseCountBack = 0;
-
-  Serial.print("Back Motor Speed: ");
-  Serial.print(rpmBack);
-  Serial.println(" RPM");
-}
 
 
-void onTimer() {
-  // Toggle LED2 state
-  ultraRead_Right();
-  ultraRead_Front();
-  publishMessage(ValueTopic, String(distanceCm));
-  publishMessage(ValueTopic2, String(distanceCmRight));
-}
 
-void calculateDistanceBack() {
-  // float wheel_curcumference = 18.84;
-  // distance = rpm * wheel_circumference;
-  // printf(distance);
-}
+
+
+
+
